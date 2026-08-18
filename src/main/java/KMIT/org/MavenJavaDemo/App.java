2cc382b4c24c9a57674fdf9538a8b5fa212d818d
NagaSharanya2007 <sharanyamanipadi@gmail.com>
2026-08-18 11:08:49 +0530

    Maven java

---
+++ src/main/java/KMIT/org/MavenJavaDemo/App.java
@@ -0,0 +1,10 @@
+package KMIT.org.MavenJavaDemo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Sharanya to SE!");
+    }
+}
